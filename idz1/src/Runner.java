579135a117dadaa83d85bb6e2308aea2330759bd
Pavel <pnikitinbox@gmail.com>
2021-05-24 14:22:06 +0300

    add array of objects

--- idz1/src/Runner.java
+++ idz1/src/Runner.java
@@ -1,6 +1,19 @@
+import com.company.Tribe;
+import java.util.ArrayList;
+
 public class Runner {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<Tribe> tribeArrayList = new ArrayList<Tribe>();
+        tribeArrayList.add(new Tribe("Dregovichi", 8100, true));
+        tribeArrayList.add(new Tribe("Radimichi", 7200, true));
+        tribeArrayList.add(new Tribe("Vyatichi", 4500, false));
+        tribeArrayList.add(new Tribe("Drevlyans", 3400, true));
+        tribeArrayList.add(new Tribe("Northerners", 8100, true));
+        tribeArrayList.add(new Tribe("Krivichi", 1600, false));
+        tribeArrayList.add(new Tribe("Volynians", 5600, true));
+        tribeArrayList.add(new Tribe("Glade", 8100, false));
+        tribeArrayList.add(new Tribe());
+        tribeArrayList.add(null);
     }
 }
