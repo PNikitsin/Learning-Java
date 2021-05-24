576a7ba70cafcb55f36ff9fe69cc3c78e704d982
Pavel <pnikitinbox@gmail.com>
2021-05-24 14:45:56 +0300

    add array objects

--- practice1/src/Runner.java
+++ practice1/src/Runner.java
@@ -1,6 +1,16 @@
-public class Runner {
+import by.gsu.pms.BusinessTrip;
 
+public class Runner {
     public static void main(String[] args) {
-	// write your code here
+        // create an array of 7 objects
+        BusinessTrip[] businessTrips = {
+                new BusinessTrip("Илья Виталиевич", 20000, 3),
+                new BusinessTrip("Владислав Петрович", 35000, 4),
+                new BusinessTrip("Даниил Михайлович", 70000, 6),
+                new BusinessTrip("Лаврентий Сергеевич", 100000, 9),
+                new BusinessTrip(),
+                null,
+                new BusinessTrip("Феликс Иванович",122000, 10),
+        };
     }
 }
