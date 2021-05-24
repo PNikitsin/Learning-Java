07c29803b9785a77ff81f96097cd455d9cfd30e8
Pavel <pnikitinbox@gmail.com>
2021-05-24 14:47:27 +0300

    add basic logic

--- practice1/src/Runner.java
+++ practice1/src/Runner.java
@@ -12,5 +12,36 @@ public class Runner {
                 null,
                 new BusinessTrip("Феликс Иванович",122000, 10),
         };
+
+        // print the array content to the console, using show() method
+        for (BusinessTrip businessTrip : businessTrips) {
+            if (businessTrip != null) {
+                businessTrip.show();
+                System.out.println();
+            }
+        }
+
+        // change the employee`s transportaion expenses for the last object of the array
+        businessTrips[6].setTransportExpenses(150000);
+        System.out.println(businessTrips[6]);
+
+        // print the duration of two initial business trips by the single operator
+        System.out.println("Duration: " + (businessTrips[1].getDays() + businessTrips[0].getDays()));
+
+        // print the array content to the console (one element per line), using toString( ) method implicitly
+        for (BusinessTrip businessTrip : businessTrips) {
+            System.out.println(businessTrip);
+        }
+
+        // find the sum of total expenses
+        int sumExpenses = 0;
+        for (BusinessTrip businessTrip : businessTrips) {
+            if (businessTrip != null && businessTrip.getTotal() > sumExpenses) {
+                sumExpenses = businessTrip.getTotal();
+            }
+        }
+
+        // find and print account name employee’s with maximum total expenses
+        System.out.println("Sum expenses: " + sumExpenses);
     }
 }
